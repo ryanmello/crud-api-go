f47563dfa41583910473c62610af58cf14339d8e
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-15 08:43:41 -0700

    create router and append movies

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import(
     "github.com/gorilla/mux"
 )
 
-// every movie has a director
 type Movie struct {
     ID string `json:"id"`
     Isbn string `json:"isbn"`
@@ -23,6 +22,22 @@ type Director struct {
     Lastname string `json:"lastname"`
 }
 
+var movies []Movie
+
 func main() {
-    
+    r := mux.NewRouter()
+
+    movies = append(movies, Movie{ ID: "1", Isbn: "438227", Title: "Movie1", Director: &Director{ Firstname: "Ryan", Lastname: "Mello" }})
+    movies = append(movies, Movie{ ID: "2", Isbn: "674291", Title: "Movie2", Director: &Director{ Firstname: "Josh", Lastname: "Uhler" }})
+    movies = append(movies, Movie{ ID: "3", Isbn: "859350", Title: "Movie3", Director: &Director{ Firstname: "Parker", Lastname: "Nelson" }})
+    movies = append(movies, Movie{ ID: "4", Isbn: "713529", Title: "Movie4", Director: &Director{ Firstname: "Brandon", Lastname: "Chamizo" }})
+
+    r.HandleFunc("/movies", getMovies).Methods("GET")
+    r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+    r.HandleFunc("/movies", createMovie).Methods("POST")
+    r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+    r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+    fmt.Printf("Starting server at port 8000\n")
+    log.Fatal(http.ListenAndServe(":8000", r))
 }
