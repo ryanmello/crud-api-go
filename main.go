669301a9971055073de2adfbfc762a1202c563ea
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-14 18:28:36 -0700

    define movie and director structs

--- main.go
+++ main.go
@@ -12,11 +12,15 @@ import(
 
 // every movie has a director
 type Movie struct {
-
+    ID string `json:"id"`
+    Isbn string `json:"isbn"`
+    Title string `json:"title"`
+    Director *Director `json:"director"`
 }
 
 type Director struct {
-
+    Firstname string `json:"firstname"`
+    Lastname string `json:"lastname"`
 }
 
 func main() {
