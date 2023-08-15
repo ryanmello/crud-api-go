64bcb237198b546d3b72cb7979357b3a68169d14
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-15 15:18:31 -0700

    create and update movie

--- main.go
+++ main.go
@@ -39,6 +39,32 @@ func getMovie(w http.ResponseWriter, r *http.Request){
     }
 }
 
+func createMovie(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    var movie Movie 
+    _ = json.NewDecoder(r.Body).Decode(&movie)
+    movie.ID = strconv.Itoa(rand.Intn(10000))
+    movies = append(movies, movie)
+    json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+    for index, item := range movies {
+        if(item.ID == params["id"]){
+            movies = append(movies[:index], movies[index + 1:]...)
+            var movie Movie 
+
+            _ = json.NewDecoder(r.Body).Decode(&movie)
+            movie.ID = params["id"]
+            movies = append(movies, movie)
+            json.NewEncoder(w).Encode(movie)
+            return
+        }
+    }
+}
+
 func deleteMovie(w http.ResponseWriter, r *http.Request){
     w.Header().Set("Content-Type", "application/json")
     params := mux.Vars(r)
@@ -48,6 +74,8 @@ func deleteMovie(w http.ResponseWriter, r *http.Request){
             break
         }
     }
+
+    json.NewEncoder(w).Encode(movies)
 }
 
 func main() {
