9c86ebbc8fd85bfdbdb708c5df12ff21cfa0dca5
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-15 09:07:06 -0700

    get movie and delete movie

--- main.go
+++ main.go
@@ -24,6 +24,22 @@ type Director struct {
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+    for index, item := range movies {
+        if(item.ID == params["id"]){
+            movies = append(movies[:index], movies[index + 1:]...)
+            break
+        }
+    }
+}
+
 func main() {
     r := mux.NewRouter()
 
